51f050063d35570ed0380c46398a6e95d59fcd0c
Octav <git@octav.info>
2016-08-31 09:15:14 +0300

    Does not matter

--- Tools/HTMLParsing.go
+++ Tools/HTMLParsing.go
@@ -7,8 +7,8 @@ func parseHTML(HTML []byte) []byte {
 	HTMLString := string(HTML)
 
 	// Replace paths
-	HTMLString = strings.Replace(HTMLString, "develop.drewberry.co.uk", "develop.drewberry.co.uk", -1)
-	HTMLString = strings.Replace(HTMLString, "drewberryinsurance.co.uk", "develop.drewberry.co.uk", -1)
+	//HTMLString = strings.Replace(HTMLString, "develop.drewberry.co.uk", "develop.drewberry.co.uk", -1)
+	HTMLString = strings.Replace(HTMLString, "www.drewberryinsurance.co.uk", "develop.drewberry.co.uk", -1)
 
 	return []byte(HTMLString)
 }
